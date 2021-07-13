9a85bd15a513b2b01ed8673f2138c6d5c1f6000d
Aldwin <aldwin828@gmail.com>
2021-07-12 23:50:20 -0400

    Advanced Array Operations and Functions

--- app.js
+++ app.js
@@ -1,30 +1,70 @@
-//Function 1
-function mathOperations(num1,num2) {
-    return [num1 + num2]*5
-}
-
-console.log(mathOperations(1,2));
-
-
-// Function 2
-function stringCount(string) {
-    if (string.length > 10){
-        return ("Input accepted");
-    } else {
-        return ("Characters should be more than 10. Please try again!");
+var tweets = [
+    {
+        tweet: "Goals allow you to control the direction of change in your favor.",
+        username: "BubblySnowflake",
+        created_at: "12-02-2019",
+        age: 51
+    },
+    {
+        tweet: "Had a great weekend",
+        username: "FairyPrincess",
+        created_at: "01-29-2019",
+        age: 20
+    },
+    {
+        tweet: "Nice weather today",
+        username: "TurboSlayer",
+        created_at: "02-02-2021",
+        age:16
+    },
+    {
+        tweet: "I don’t have a bad handwriting, I have my own font.",
+        username: "experthead",
+        created_at: "12-01-2020",
+        age: 17
+    },
+    {
+        tweet: "Reading texts half asleep is like looking into the sun.",
+        username: "dyzzie",
+        created_at: "03-12-2021",
+        age: 30
+    },
+    {
+        tweet: "If opportunity doesn’t knock, build a door.",
+        username: "happee",
+        created_at: "01-26-2021",
+        age: 26
+    },
+    {
+        tweet: "When you get to the end of the rope, tie a knot and hang on.",
+        username: "o0o_jean_o0o",
+        created_at: "05-16-2021",
+        age: 16
+    },
+    {
+        tweet: "Nothing is impossible, the word itself says I am possible!",
+        username: "eatingcake",
+        created_at: "08-18-2020",
+        age: 27
+    },
+    {
+        tweet: "If you cannot do great things, do small things in a great way.",
+        username: "ishot",
+        created_at: "11-12-2020",
+        age: 17
+    },
+    {
+        tweet: "You are never too old to set another goal or to dream a new dream.",
+        username: "citrus",
+        created_at: "04-22-2021",
+        age: 18,
     }
-}
+];
 
-console.log(stringCount("Lorem ipsum dolor sit amet consectetur adipisicing elit."));
-
-
-// Function 3
-var list = ["john", "jack", "philip", "peter", "phoebe"];
-
-function startswith(name) {
-return name.startsWith("ph");
-}
-
-var result = list.filter(startswith);
+function ageOver(user) {
+    return user.age >= 18;
+    }
+    
+var result = tweets.filter(ageOver);
 
 console.log(result);
